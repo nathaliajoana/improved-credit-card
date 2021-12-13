f5814b2d13f39e89ee5d28b499c179c092cb940f
Nathália Joana <88217604+nathaliajoana@users.noreply.github.com>
2021-12-13 16:14:24 -0300

    logic update

--- src/cc/logic/nova_compra.clj
+++
@@ -1,8 +0,0 @@
-(ns cc.logic.nova-compra
-  (:require [cc.model :as m]
-            [schema.core :as s]))
-
-(s/defn adiciona-compra :- m/Compras
-  [compras :- m/Compras, compra :- m/Compra]
-  (let [id (:id-compra compra)]
-    (assoc compras id compra)))
